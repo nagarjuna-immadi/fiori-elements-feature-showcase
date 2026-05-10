84043cdd7e54da8e6f2976ceed8eabe68398c925
Nagarjuna Immadi <nagarjuna.immadi@real-digital.de>
2026-05-10 13:01:49 +0200

    Add controller extension for RootEntity in List Report and update manifest

---
+++ app/v2listreport-objectpage/webapp/ext/controller/RootEntityLRExtension.controller.ts
@@ -0,0 +1,20 @@
+import ControllerExtension from 'sap/ui/core/mvc/ControllerExtension';
+import ExtensionAPI from 'sap/fe/templates/ListReport/ExtensionAPI';
+
+/**
+ * @namespace v2.listreport.objectpage.v2listreportobjectpage.ext.controller
+ * @controller
+ */
+export default class RootEntityLRExtension extends ControllerExtension<ExtensionAPI> {
+	static overrides = {
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf v2.listreport.objectpage.v2listreportobjectpage.ext.controller.RootEntityLRExtension
+		 */
+		onInit(this: RootEntityLRExtension) {
+			// you can access the Fiori elements extensionAPI via this.base.getExtensionAPI
+			const model = this.base.getExtensionAPI().getModel();
+		}
+	}
+}
